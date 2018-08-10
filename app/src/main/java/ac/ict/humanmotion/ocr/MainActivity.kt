75edb2ba250e5816142e6a1a32de43a95bdbf50e
1996scarlet@gmail.com <1996scarlet@gmail.com>
2018-08-10 12:03:13 +0800

    final fix

--- app/src/main/java/ac/ict/humanmotion/ocr/MainActivity.kt
+++ app/src/main/java/ac/ict/humanmotion/ocr/MainActivity.kt
@@ -1,16 +1,16 @@
 package ac.ict.humanmotion.ocr
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.annotation.TargetApi
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
-import android.os.Handler
-import android.os.Message
+import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.View
 import com.googlecode.tesseract.android.TessBaseAPI
 import kotlinx.android.synthetic.main.activity_main.*
 import org.opencv.android.Utils
@@ -25,55 +25,78 @@ import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var diagram: Bitmap
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            CROP_REQUEST_CODE -> setUriToView(outUri)
+            CAMERA_REQUEST_CODE -> clipPhoto()
+        }
+    }
 
-    lateinit var srcMat: Mat
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         getStorageAccessPermissions()
+    }
 
-        val tessBaseAPI = TessBaseAPI()
-        tessBaseAPI.init("/storage/emulated/0/", "chi_sim")
+    private fun setUriToView(uri: Uri = outUri) {
+        println("using uri")
+        diagram = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
+        imageView.setImageBitmap(diagram)
+        OCRthread()
+    }
 
-        val handle = @SuppressLint("HandlerLeak")
-        object : Handler() {
-            override fun handleMessage(msg: Message?) {
-                msg.let {
-                    when (it?.obj) {
-                        BITMAP_TAG -> {
-                            imageView.setImageBitmap(diagram)
-                        }
+    fun cameraCapture(view: View) = startActivityForResult(Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            .putExtra(MediaStore.EXTRA_OUTPUT, imageUri), CAMERA_REQUEST_CODE)
 
-                        else -> {
-                        }
-                    }
-                }
-            }
+
+    private fun clipPhoto(uri: Uri = imageUri) {
+        startActivityForResult(Intent("com.android.camera.action.CROP")
+                .setDataAndType(uri, "image/*")
+                .putExtra("crop", "true")
+                .putExtra(MediaStore.EXTRA_OUTPUT, outUri)
+                .putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString())
+                .putExtra("return-data", false), CROP_REQUEST_CODE)
+    }
+
+    private fun setPicToView(picdata: Intent) {
+        println("using intent")
+        picdata.extras?.getParcelable<Bitmap>("data")?.let {
+            diagram = it
+            imageView.setImageBitmap(diagram)
+
+            OCRthread()
         }
+    }
+
+    private fun OCRthread() {
+
 
         thread {
 
-            srcMat = convertBitMap2Mat(BitmapFactory.decodeFile("/storage/emulated/0/tessdata/camera.jpg"))
+            val tessBaseAPI = TessBaseAPI()
+            tessBaseAPI.init("/storage/emulated/0/", "chi_sim")
+
+            val resultList = mutableListOf("OCR Result:\n")
+
+            srcMat = convertBitMap2Mat(diagram)
             val temp = Mat()
 
             cvtColor(srcMat, temp, COLOR_RGBA2GRAY)
             Sobel(temp, temp, CV_8U, 1, 0)
+            Sobel(temp, temp, CV_8U, 0, 1)
             threshold(temp, temp, 0.0, 255.0, THRESH_OTSU + THRESH_BINARY)
 
-            val element1 = getStructuringElement(MORPH_RECT, Size(30.0, 9.0))
+            val element1 = getStructuringElement(MORPH_RECT, Size(30.0, 2.0))
             val element2 = getStructuringElement(MORPH_RECT, Size(24.0, 3.0))
 
             dilate(temp, temp, element2)
-            dilate(temp, temp, element2)
 
-//            erode(temp, temp, element1)
+            erode(temp, temp, element1)
 
-            dilate(temp, temp, element2)
-            dilate(temp, temp, element2)
-
+            for (i in 0..8) dilate(temp, temp, element2)
 
             val rects: MutableList<RotatedRect> = mutableListOf()
             val contours: MutableList<MatOfPoint> = mutableListOf()
@@ -83,7 +106,7 @@ class MainActivity : AppCompatActivity() {
             contours.forEach {
                 val area = contourArea(it)
 
-                if (area < 1000) return@forEach
+                if (area < 8000) return@forEach
 
                 val newPoint = MatOfPoint2f()
                 it.convertTo(newPoint, CvType.CV_32F)
@@ -107,25 +130,55 @@ class MainActivity : AppCompatActivity() {
                 val pt = arrayOf(Point(), Point(), Point(), Point())
                 it.points(pt)
 
-                for (j in 0..3) {
-                    line(srcMat, pt[j], pt[(j + 1) % 4], Scalar(0.0, 255.0, 0.0), 2)
-                }
-
+                for (j in 0..3) line(srcMat, pt[j], pt[(j + 1) % 4], Scalar(0.0, 255.0, 0.0), 2)
             }
 
             diagram = converMat2Bitmat(srcMat)
 
+            runOnUiThread {
+                imageView.setImageBitmap(diagram)
+            }
+
             saveResult("/storage/emulated/0/tessdata/result.jpg")
 
-            val msg = Message.obtain()
-            msg.obj = BITMAP_TAG
-            handle.sendMessage(msg)
+            cvtColor(srcMat, temp, COLOR_RGBA2GRAY)
 
-            rects.forEach {
-                val src = converMat2Bitmat(cropMatByRoi(srcMat, it))
-                tessBaseAPI.setImage(src)
-                println(tessBaseAPI.utF8Text)
+            rects.asReversed().forEachIndexed { index, it ->
+                tessBaseAPI.setImage(converMat2Bitmat(cropMatByRoi(temp, it)))
+
+                var res = ""
+
+                for (t in tessBaseAPI.utF8Text) {
+                    if (!rex.contains(t)) res += t
+                }
+
+                resultList.add(res)
+                println(index.toString() + "-" + res)
             }
+
+//            println(resultList)
+            runOnUiThread {
+                textView.text = ""
+                resultList.forEach {
+                    textView.append(it)
+                }
+            }
+
+            //            val executor = Executors.newFixedThreadPool(rects.size)
+
+
+//            rects.forEach {it: RotatedRect ->
+//                //                val worker = Runnable {
+////                    tessBaseAPI.setImage(converMat2Bitmat(cropMatByRoi(srcMat, it)))
+////                    println(tessBaseAPI.utF8Text)
+////                }
+////                executor.execute(worker)
+//
+//                tessBaseAPI.setImage(converMat2Bitmat(cropMatByRoi(srcMat, it)))
+//                println("${tessBaseAPI.utF8Text}")
+//            }
+
+            tessBaseAPI.end()
         }
     }
 
@@ -187,18 +240,30 @@ class MainActivity : AppCompatActivity() {
 
         const val PERMISSION_TAG = "RequestPermissions"
         const val BITMAP_TAG = "DONE"
+        const val CAMERA_REQUEST_CODE = 10086
+        const val CROP_REQUEST_CODE = 10085
+        const val RES_REQUEST_CODE = 10000
+        const val rex = "/*-+)(<>'\\~!@$%&^ -:;[]{}「『…【】_《》oo′\"`\'“”‘’,."
+
+        val imageUri = Uri.parse("file:///storage/emulated/0/tessdata/temp.jpg")
+        val outUri = Uri.parse("file:///storage/emulated/0/tessdata/output.jpg")
     }
+
+    lateinit var diagram: Bitmap
+    lateinit var srcMat: Mat
+//    lateinit var tessBaseAPI: TessBaseAPI
 
     @TargetApi(23)
     private fun getStorageAccessPermissions() {
-        requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE), 10086)
+        requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE), RES_REQUEST_CODE)
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         println("$PERMISSION_TAG:onRequestPermissionsResult: ${grantResults[0]}")
         when (requestCode) {
-            10086 -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) println("$PERMISSION_TAG:permission get!") else println("$PERMISSION_TAG:permission denied! ")
-            else -> {
+            RES_REQUEST_CODE -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) println("$PERMISSION_TAG:permission get!") else {
+                println("$PERMISSION_TAG:permission denied! ")
+                finish()
             }
         }
     }
