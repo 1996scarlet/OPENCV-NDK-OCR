a01fac1d711afc7b010e0aa9a9c63eb263cddaab
1996scarlet@gmail.com <1996scarlet@gmail.com>
2018-08-13 21:14:36 +0800

    final fix

---
+++ app/src/main/java/ac/ict/humanmotion/ocr/TrackerActivity.kt
@@ -0,0 +1,12 @@
+package ac.ict.humanmotion.ocr
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TrackerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tracker)
+    }
+}
